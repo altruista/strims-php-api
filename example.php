c14ef59820986e6db55d05fee63ca3de721151aa
altruista <aaltruista@gmail.com>
2013-05-06 23:41:32 +0100

    wersja 1.0

---
+++ example.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once "Strims.class.php";
+
+header("Content-type:text/plain;charset=utf-8");
+$strims = new Strims();
+
+$entries = $strims->get_entries('u/altruista');
+
+if($strims->login('uzytkownik', 'haslo')) {
+	$strims->post_entry('Ciekawostki', 'ciekawy wpis');
+	$strims->post_link('Ciekawostki', 'ciekawy link', 'http://google.pl');
+}
+
