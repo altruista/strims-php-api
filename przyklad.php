85e84057de1fa5b44c9b869c4cf563cd50783a12
altruista <aaltruista@gmail.com>
2013-06-25 19:25:34 +0100

    v1.05b

--- przyklad.php
+++ przyklad.php
@@ -6,9 +6,9 @@ $strims = new Strims();
 
 $entries = $strims->get_entries('u/altruista');
 
-if($strims->login('uzytkownik', 'haslo')) {
-	$strims->post_entry('Ciekawostki', 'ciekawy wpis');
-	$strims->post_link('Ciekawostki', 'ciekawy link', 'http://google.pl');
+if ($strims->login('uzytkownik', 'haslo')) {
+    $strims->post_entry('Ciekawostki', 'ciekawy wpis');
+    $strims->post_link('Ciekawostki', 'ciekawy link', 'http://google.pl');
 }
 
 
